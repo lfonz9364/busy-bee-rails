b659e55305875d7812eae8961057bb09d3644f03
Alfons Caroles <cllin7787@gmail.com>
2026-03-02 17:58:29 +1100

    Updated feedback model to represent current feedback table

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,9 +1,8 @@
 class Feedback < ApplicationRecord
-  belongs_to :client
-  belongs_to :developer
+  belongs_to :user
   belongs_to :job
 
-  validates :body, presence: true
+  validates :comment, presence: true
   validates :rating, 
             numericality: { only_integer: true, in: 1..5 }, 
             allow_nil: true
