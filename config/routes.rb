685b55f47221a151695256e51de1b5400778c07d
Alfons Caroles <cllin7787@gmail.com>
2026-03-10 15:03:18 +1100

    Completed routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "jobs#index"
 
-  resources :developers,  only: [:show, :index]
-  resources :clients,     only: [:show, :index]
-
-
+  get "/login", to: "sessions#new", as: :login
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: :logout
+  
+  resources :developers,  only: [:index, :show]
+  resources :clients,     only: [:index, :show]
+  resources :users,       only: [:index, :show]
+  resources :jobs
 end
