eeed1e562d5706d93fd7480af334d915728eee54
Alfons Caroles <cllin7787@gmail.com>
2026-03-16 21:56:39 +1100

    Add jobs controller protection

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,61 @@
 class JobsController < ApplicationController
+  before_action :require_login
   before_action :set_job, only: :show
+  before_action :require_client, only: %i[new create edit update destroy]
+  before_action :authorise_client_job_edit!, only: %i[edit update destroy]
 
   def index
-    @jobs = Job.includes(:developer, :client)
+    @jobs = Job.includes(developer: :user, client: :user).order(created_at: :desc)
+
+    if params[:query].present?
+      q = "%#{params[:query]}%"
+      @jobs = @jobs.where("title ILIKE ? OR description ILIKE ?", q, q)
+    end
   end
 
   def show
     @feedbacks = @job.feedbacks.includes(:client).order(created_at: :desc)
   end
 
+  def create
+    @job = current_user.client.jobs.build(job_params)
+
+    if @job.save
+      redirect_to @job, notices: "Job created succesfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to @job, notice: "Job updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path, notice: "Job deleted succesfully"
+  end
+
   private 
 
   def set_job
     @job = Job.find(params[:id])
+  end
+
+  def authorise_client_job_edit!
+    return if @job.editable_by_client?(current_user)
+
+    redirect_to @job, alert: "You are not allowed to modify this job"
+  end
+
+  def job_params
+    params.require(:job).permit(:title, :description, :reward, :status, :deadline)
   end
 end
