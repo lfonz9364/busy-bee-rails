f9569c1e8011f3e546f2bc639aa59ecc155d7daf
Alfons Caroles <cllin7787@gmail.com>
2026-03-18 16:42:58 +1100

    Adding test to ensure only admin could assign other user as admin

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -65,4 +65,28 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
   end
+
+  test "admin can make another user admin" do
+    admin = create_user(admin: true)
+    user = create_user(admin: false)
+
+    sign_in_as(admin)
+
+    patch make_admin_user_url(user)
+
+    assert_redirected_to users_path
+    assert user.reload.admin?
+  end
+
+  test "non admin cannot make another user admin" do
+    user = create_user(admin: false)
+    other_user = create_user(admin: false)
+
+    sign_in_as(user)
+
+    patch make_admin_user_url(other_user)
+
+    assert_redirected_to root_path
+    assert_not other_user.reload.admin?
+  end
 end
