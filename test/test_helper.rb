bce722f00e1143eef6bf654ec7ef28df4b24bf39
Alfons Caroles <cllin7787@gmail.com>
2026-03-25 15:18:12 +1100

    Revert unecessary changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require "active_job/test_helper"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  parallelize(workers: 1)
   # Add more helper methods to be used by all tests here...
   def create_user(overrides = {})
     defaults = {
@@ -98,8 +96,6 @@ class ActiveSupport::TestCase
 end
 
 class ActionDispatch::IntegrationTest
-  include ActiveJob::TestHelper
-  
   def sign_in_as(user, password: "password123")
     post login_url, params: {
       email: user.email,
